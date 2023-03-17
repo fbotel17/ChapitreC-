3abcc004e15c29c76f3cba6dee6bff5a262afb12
fbotel17 <faustbtl17@gmail.com>
2023-03-17 09:56:28 +0100

    envoi en server

--- QtWidgetsApplication1/QtWidgetsApplication1/QtWidgetsApplication1.cpp
+++ QtWidgetsApplication1/QtWidgetsApplication1/QtWidgetsApplication1.cpp
@@ -7,6 +7,12 @@ QtWidgetsApplication1::QtWidgetsApplication1(QWidget *parent)
     socket = new QTcpSocket(this);
     QObject::connect(socket, SIGNAL(connected()), this, SLOT(onSocketConnected()));
     QObject::connect(socket, SIGNAL(disconnected()), this, SLOT(onSocketDisconnected()));
+    QObject::connect(socket, SIGNAL(readyRead()), this, SLOT(onSocketReadyRead()));
+
+    server = new QTcpServer(this);
+    QObject::connect(server, SIGNAL(newConnection()), this, SLOT(onServerNewConnection()));
+    server->listen(QHostAddress::AnyIPv4, 4321);
+
 }
 
 
@@ -39,4 +45,38 @@ void QtWidgetsApplication1::onSendMessageButonClicket()
     {
         socket->write("Hello server !\n");
     }
+    
+}
+
+void QtWidgetsApplication1::onSocketReadyRead()
+{
+    QByteArray data = socket->read(socket->bytesAvailable());
+    QString str(data);
+    ui.connectionStatusLabel->setText("Status connexion : Message recu : "+str);
+}
+
+void QtWidgetsApplication1::onServerNewConnection()
+{
+    ui.connectionStatusLabel->setText("Un client s'est connecté");
+    QTcpSocket* client = server->nextPendingConnection();
+    QObject::connect(client, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
+    QObject::connect(client, SIGNAL(disconnected()), this, SLOT(onClientDisconnected()));
+}
+
+void QtWidgetsApplication1::onClientDisconnected()
+{
+    QTcpSocket* obj = qobject_cast<QTcpSocket*>(sender());
+    QObject::disconnect(obj, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
+    QObject::disconnect(obj, SIGNAL(disconnected()), this, SLOT(onClientDisconnected()));
+    obj->deleteLater();
+    ui.connectionStatusLabel->setText("Un client s'est déconnecté");
+
+}
+
+void QtWidgetsApplication1::onClientReadyRead()
+{
+    QTcpSocket* obj = qobject_cast<QTcpSocket*>(sender());
+    QByteArray data = obj->read(obj->bytesAvailable());
+    QString str(data);
+    ui.connectionStatusLabel->setText(str);
 }
--- QtWidgetsApplication1/QtWidgetsApplication1/QtWidgetsApplication1.h
+++ QtWidgetsApplication1/QtWidgetsApplication1/QtWidgetsApplication1.h
@@ -3,6 +3,7 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_QtWidgetsApplication1.h"
 #include <qtcpsocket.h>
+#include <qtcpserver.h>
 
 class QtWidgetsApplication1 : public QMainWindow
 {
@@ -14,10 +15,16 @@ public:
 private:
     Ui::QtWidgetsApplication1Class ui;
     QTcpSocket* socket;
+    QTcpServer* server;
 
 public slots:
     void onDisplayMessageButtonClicket();
     void onSocketConnected();
     void onSocketDisconnected();
     void onSendMessageButonClicket();
+    void onSocketReadyRead();
+
+    void onServerNewConnection();
+    void onClientDisconnected();
+    void onClientReadyRead();
 };
